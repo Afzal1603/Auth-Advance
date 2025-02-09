b0ffc5da2708816a5b4b5a4124a2ff0544f1fdd8
Mohd Afzal Ansari <afzalansaria660@gmail.com>
2025-02-09 21:17:00 +0530

    change

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -114,7 +114,11 @@ const login = async (req, res) => {
 };
 
 const logout = async (req, res) => {
-  res.clearCookie("token");
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "Strict",
+  });
   res.status(200).json({ success: true, message: "Logged out successfully" });
 };
 
